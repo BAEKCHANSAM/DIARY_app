9b9be02aaa52cef6cdf72caac15a5c7b83cac2a0
cksto <ckstoa100@naver.com>
2021-04-27 13:12:12 +0900

    sussess

--- app/src/main/java/com/example/ins_copy/Sign_Up.java
+++ app/src/main/java/com/example/ins_copy/Sign_Up.java
@@ -1,32 +1,76 @@
 package com.example.ins_copy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Sign_Up extends AppCompatActivity {
+    private static final  String TAG = "SignUpActivity";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
         ActionBar actionBar = getSupportActionBar();actionBar.hide();//위에 액션바를 없애주는 코드
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         findViewById(R.id.signUpButton).setOnClickListener(onClickListener );
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
       switch (v.getId()){
           case R.id.signUpButton:
-
+              signup();
               break;
           }
         }
     };
+
+    private void signup(){
+
+        String email = ((EditText)findViewById(R.id.emailEditText)).getText().toString();
+        String password = ((EditText)findViewById(R.id.passwordEditTexts)).getText().toString();
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //성공했을떄 Ui로직
+                        } else {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            //실패 했을떄 UI로직
+                        }
+                    }
+                });
+    }
+
 }
